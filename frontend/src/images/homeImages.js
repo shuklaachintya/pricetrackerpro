733b3996e40d985d719ca9d8722b4edb5404f32b
Achintya Shukla <achintshukla@gmail.com>
2024-03-12 19:34:06 +0530

    completed initial version of Price TRacker

---
+++ frontend/src/images/homeImages.js
@@ -0,0 +1,8 @@
+module.exports = {
+  heroBg:
+    "https://res.cloudinary.com/dnkkw3nqk/image/upload/v1601946477/trackerbase/clouds1_s7zehg.png",
+  heroVector:
+    "https://res.cloudinary.com/dnkkw3nqk/image/upload/v1601946500/trackerbase/slides1_linufl.png",
+  featureItemBg:
+    "https://res.cloudinary.com/dnkkw3nqk/image/upload/v1601946517/trackerbase/clouds9_vprvms.png",
+};
